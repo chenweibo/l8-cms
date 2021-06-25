4b19c63ad47ae9794a7fc42e900b252e85bbe3e9
chenweibo <563960993@qq.com>
2021-06-25 16:13:32 +0800

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -11,70 +11,116 @@
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <div class="flex items-start justify-center min-h-screen pt-5 bg-white min-w-screen">
                         <div class="flex flex-wrap w-full px-4">
-                            <div class="w-full px-2 sm:w-1/2 xl:w-1/3">
+                            <div class="w-full mb-4 px-2 sm:w-1/2 xl:w-1/3">
                                 <div
                                     class="relative flex items-center px-5 py-6 overflow-hidden rounded-md shadow-sm bg-gradient-to-br from-gray-100 to-gray-200">
-                                    <span class="absolute top-0 left-0 w-2 h-full bg-gradient-to-b from-indigo-300 to-indigo-500"></span>
-                                    <div class="p-3 bg-opacity-75 rounded-full bg-gradient-to-br from-indigo-400 to-indigo-600">
-                                        <svg class="w-8 h-8 text-white" viewBox="0 0 28 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M18.2 9.08889C18.2 11.5373 16.3196 13.5222 14 13.5222C11.6804 13.5222 9.79999 11.5373 9.79999 9.08889C9.79999 6.64043 11.6804 4.65556 14 4.65556C16.3196 4.65556 18.2 6.64043 18.2 9.08889Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M25.2 12.0444C25.2 13.6768 23.9464 15 22.4 15C20.8536 15 19.6 13.6768 19.6 12.0444C19.6 10.4121 20.8536 9.08889 22.4 9.08889C23.9464 9.08889 25.2 10.4121 25.2 12.0444Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M19.6 22.3889C19.6 19.1243 17.0927 16.4778 14 16.4778C10.9072 16.4778 8.39999 19.1243 8.39999 22.3889V26.8222H19.6V22.3889Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M8.39999 12.0444C8.39999 13.6768 7.14639 15 5.59999 15C4.05359 15 2.79999 13.6768 2.79999 12.0444C2.79999 10.4121 4.05359 9.08889 5.59999 9.08889C7.14639 9.08889 8.39999 10.4121 8.39999 12.0444Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M22.4 26.8222V22.3889C22.4 20.8312 22.0195 19.3671 21.351 18.0949C21.6863 18.0039 22.0378 17.9556 22.4 17.9556C24.7197 17.9556 26.6 19.9404 26.6 22.3889V26.8222H22.4Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M6.64896 18.0949C5.98058 19.3671 5.59999 20.8312 5.59999 22.3889V26.8222H1.39999V22.3889C1.39999 19.9404 3.2804 17.9556 5.59999 17.9556C5.96219 17.9556 6.31367 18.0039 6.64896 18.0949Z"
-                                                fill="currentColor" />
+                                    <span class="absolute top-0 left-0 w-2 h-full bg-gray-500"></span>
+                                    <div class="p-3 rounded-full  bg-gray-700">
+                                        <svg class="h-8 w-8 text-white" fill="none" stroke="currentColor"
+                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" stroke-linecap="round" stroke-linejoin="round"
+                                                  stroke-width="2"/>
                                         </svg>
                                     </div>
                                     <div class="mx-5">
                                         <h4 class="text-2xl font-semibold text-gray-700">10</h4>
-                                        <div class="text-gray-500">新用户</div>
+                                        <div class="text-gray-500">管理员数量</div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="w-full px-2 mt-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
+                            <div class="w-full mb-4 px-2 sm:w-1/2 xl:w-1/3">
                                 <div
                                     class="relative flex items-center px-5 py-6 overflow-hidden rounded-md shadow-sm bg-gradient-to-br from-gray-100 to-gray-200">
-                                    <span class="absolute top-0 left-0 w-2 h-full bg-gradient-to-b from-pink-300 to-pink-500"></span>
-                                    <div class="p-3 bg-opacity-75 rounded-full bg-gradient-to-br from-pink-400 to-pink-600">
-                                        <svg class="w-8 h-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor"
-                                                  stroke="currentColor" stroke-width="2" stroke-linejoin="round" />
+                                    <span
+                                        class="absolute top-0 left-0 w-2 h-full bg-gradient-to-b from-indigo-300 to-indigo-500"></span>
+                                    <div
+                                        class="p-3 bg-opacity-75 rounded-full bg-gradient-to-br from-indigo-400 to-indigo-600">
+
+                                        <svg class="w-8 h-8 text-white" fill="none"
+                                             stroke="currentColor" viewBox="-2 0 28 26" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" stroke-linecap="round" stroke-linejoin="round"
+                                                  stroke-width="2"/>
+                                        </svg>
+                                    </div>
+                                    <div class="mx-5">
+                                        <h4 class="text-2xl font-semibold text-gray-700">10</h4>
+                                        <div class="text-gray-500">单篇文章</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="w-full mb-4 px-2 mt-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
+                                <div
+                                    class="relative flex items-center px-5 py-6 overflow-hidden rounded-md shadow-sm bg-gradient-to-br from-gray-100 to-gray-200">
+                                    <span
+                                        class="absolute top-0 left-0 w-2 h-full bg-gradient-to-b from-pink-300 to-pink-500"></span>
+                                    <div
+                                        class="p-3 bg-opacity-75 rounded-full bg-gradient-to-br from-pink-400 to-pink-600">
+                                        <svg class="w-8 h-8 text-white" fill="none" viewBox="0 0 28 28"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z"
+                                                  fill="currentColor"
+                                                  stroke="currentColor" stroke-linejoin="round" stroke-width="2"/>
                                             <path
                                                 d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z"
-                                                stroke="currentColor" stroke-width="2" />
+                                                stroke="currentColor" stroke-width="2"/>
                                         </svg>
                                     </div>
                                     <div class="mx-5">
                                         <h4 class="text-2xl font-semibold text-gray-700">20</h4>
-                                        <div class="text-gray-500">内容</div>
+                                        <div class="text-gray-500">列表内容</div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="w-full px-2 mt-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
+                            <div class="w-full mb-4 px-2 mt-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
                                 <div
                                     class="relative flex items-center px-8 py-6 overflow-hidden rounded-md shadow-sm bg-gradient-to-br from-gray-100 to-gray-200">
-                                    <span class="absolute top-0 left-0 w-2 h-full bg-gradient-to-b from-orange-300 to-orange-500"></span>
-                                    <div class="p-3 bg-opacity-75 rounded-full bg-gradient-to-br from-orange-400 to-orange-600">
-                                        <svg class="w-8 h-8 text-white" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linejoin="round"
-                                                  d="M1664 1504v-768q-32 36-69 66-268 206-426 338-51 43-83 67t-86.5 48.5-102.5 24.5h-2q-48 0-102.5-24.5t-86.5-48.5-83-67q-158-132-426-338-37-30-69-66v768q0 13 9.5 22.5t22.5 9.5h1472q13 0 22.5-9.5t9.5-22.5zm0-1051v-24.5l-.5-13-3-12.5-5.5-9-9-7.5-14-2.5h-1472q-13 0-22.5 9.5t-9.5 22.5q0 168 147 284 193 152 401 317 6 5 35 29.5t46 37.5 44.5 31.5 50.5 27.5 43 9h2q20 0 43-9t50.5-27.5 44.5-31.5 46-37.5 35-29.5q208-165 401-317 54-43 100.5-115.5t46.5-131.5zm128-37v1088q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-1088q0-66 47-113t113-47h1472q66 0 113 47t47 113z" />
+                                    <span class="absolute top-0 left-0 w-2 h-full bg-green-400"></span>
+                                    <div class="p-3  rounded-full bg-green-400">
+                                        <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor"
+                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76" stroke-linecap="round" stroke-linejoin="round"
+                                                  stroke-width="2"/>
                                         </svg>
                                     </div>
                                     <div class="mx-5">
                                         <h4 class="text-2xl font-semibold text-gray-700">900</h4>
-                                        <div class="text-gray-500">订单</div>
+                                        <div class="text-gray-500">留言</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full mb-4 px-2 sm:w-1/2 xl:w-1/3">
+                                <div
+                                    class="relative flex items-center px-5 py-6 overflow-hidden rounded-md shadow-sm bg-gradient-to-br from-gray-100 to-gray-200">
+                                    <span class="absolute top-0 left-0 w-2 h-full bg-red-600"></span>
+                                    <div class="p-3  rounded-full bg-red-600">
+                                        <svg class="h-8 w-8 text-white" fill="none" stroke="currentColor"
+                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" stroke-linecap="round" stroke-linejoin="round"
+                                                  stroke-width="2"/>
+                                        </svg>
+                                    </div>
+                                    <div class="mx-5">
+                                        <h4 class="text-2xl font-semibold text-gray-700">10</h4>
+                                        <div class="text-gray-500">未读留言</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full mb-4 px-2 sm:w-1/2 xl:w-1/3">
+                                <div
+                                    class="relative flex items-center px-5 py-6 overflow-hidden rounded-md shadow-sm bg-gradient-to-br from-gray-100 to-gray-200">
+                                    <span class="absolute top-0 left-0 w-2 h-full bg-black"></span>
+                                    <div class="p-3  rounded-full bg-black">
+                                        <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor"
+                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" stroke-linecap="round" stroke-linejoin="round"
+                                                  stroke-width="2"/>
+                                        </svg>
+
+                                    </div>
+                                    <div class="mx-5">
+                                        <h4 class="text-2xl font-semibold text-gray-700">10</h4>
+                                        <div class="text-gray-500">自定义组件</div>
                                     </div>
                                 </div>
                             </div>
@@ -88,13 +134,13 @@
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout'
-    import Welcome from '@/Jetstream/Welcome'
+import AppLayout from '@/Layouts/AppLayout'
+import Welcome from '@/Jetstream/Welcome'
 
-    export default {
-        components: {
-            AppLayout,
-            Welcome,
-        },
-    }
+export default {
+    components: {
+        AppLayout,
+        Welcome,
+    },
+}
 </script>
